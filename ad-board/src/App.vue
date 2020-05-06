8968e6d9175c40bc7b00fd546e123e99e5f05cdb
ayurkevich <artur.yurkevich.it@gmail.com>
2020-05-05 20:38:29 -0400

    links for navigation were added

--- ad-board/src/App.vue
+++ ad-board/src/App.vue
@@ -5,20 +5,17 @@
       app
     >
       <v-list dense>
-        <v-list-item link>
+        <v-list-item
+          v-for="link of links"
+          :key="link.title"
+          link
+          :to="link.url"
+        >
           <v-list-item-action>
-            <v-icon>mdi-home</v-icon>
+            <v-icon>{{link.icon}}</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-contact-mail</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Contact</v-list-item-title>
+            <v-list-item-title v-text="link.title">Home</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
@@ -30,23 +27,19 @@
       dark
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title>Ad Board</v-toolbar-title>
       <v-spacer></v-spacer>
 
       <v-btn
-        class="ma-2"
+        v-for="link of links"
+        :key="link.title"
+        :to="link.url"
+        link
+        class="ma-2 hidden-sm-and-down"
         outlined
         color="white">
-          Button 1
+        {{link.title}}
       </v-btn>
-
-      <v-btn
-        class="ma-2"
-        outlined
-        color="white">
-          Button 2
-      </v-btn>
-
     </v-app-bar>
 
     <v-content>
@@ -56,7 +49,7 @@
       color="indigo"
       app
     >
-      <span class="white--text">&copy; 2019</span>
+      <span class="white--text">&copy; 2020</span>
     </v-footer>
   </v-app>
 </template>
@@ -68,6 +61,14 @@
     },
     data: () => ({
       drawer: null,
+      links: [
+        {title: 'Home', icon: 'mdi-home', url: '/'},
+        {title: 'Login', icon: 'mdi-lock', url: '/login'},
+        {title: 'Registration', icon: 'mdi-face', url: '/registration'},
+        {title: 'Orders', icon: 'mdi-bookmark', url: '/orders'},
+        {title: 'New ad', icon: 'mdi-plus-circle-outline', url: '/new'},
+        {title: 'My ads', icon: 'mdi-clipboard-list-outline', url: '/list'}
+      ]
     }),
   }
 </script>
