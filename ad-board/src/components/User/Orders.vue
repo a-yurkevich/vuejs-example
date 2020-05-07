7755d7bd54e7a65a6c7ad64c5cfb8392e661e2db
ayurkevich <artur.yurkevich.it@gmail.com>
2020-05-06 21:47:32 -0400

    Order page

--- ad-board/src/components/User/Orders.vue
+++ ad-board/src/components/User/Orders.vue
@@ -1,8 +1,44 @@
 <template>
   <v-container>
-    <v-layout>
-      <v-flex>
-        <h1>Orders</h1>
+    <v-layout row>
+      <v-flex xs12 md6>
+        <h1 class="text--secondary">Orders</h1>
+        <v-list
+          subheader
+          two-line
+          flat
+        >
+          <v-list-item-group
+            multiple
+            v-for="order in orders"
+            :key="order.id"
+          >
+            <v-list-item>
+              <template v-slot:default="{ active, toggle }">
+                <v-list-item-action>
+                  <v-checkbox
+                    :input-value="order.done"
+                    v-model="active"
+                    color="primary"
+                    @change="markDone(order)"
+                  ></v-checkbox>
+                </v-list-item-action>
+
+                <v-list-item-content>
+                  <v-list-item-title>{{order.name}}</v-list-item-title>
+                  <v-list-item-subtitle>{{order.phone}}</v-list-item-subtitle>
+                </v-list-item-content>
+                <v-list-item-action>
+                  <v-btn
+                    class="indigo"
+                    dark
+                    :to="'/ad/' + order.id"
+                  >Open</v-btn>
+                </v-list-item-action>
+              </template>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
       </v-flex>
     </v-layout>
   </v-container>
@@ -12,7 +48,20 @@
   export default {
     data () {
       return {
-
+        orders: [
+          {
+            id: '1',
+            name: 'Artur',
+            phone: '+37529-793-2718',
+            adId: '1',
+            done: false
+          }
+        ]
+      }
+    },
+    methods: {
+      markDone (order) {
+        order.done = true
       }
     }
   }
