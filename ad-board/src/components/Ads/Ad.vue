39bcc47fcc023de3085800ade3e35a0f4d661838
ayurkevich <artur.yurkevich.it@gmail.com>
2020-05-07 08:25:10 -0400

    Ad page

--- ad-board/src/components/Ads/Ad.vue
+++ ad-board/src/components/Ads/Ad.vue
@@ -1,8 +1,27 @@
 <template>
   <v-container>
-    <v-layout>
-      <v-flex>
-        <h1>Ad</h1>
+    <v-layout row>
+      <v-flex xs12>
+        <h1 class="text--secondary mb-3">Ad</h1>
+        <v-card>
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/carousel/bird.jpg"
+            height="300px"
+          >
+          </v-img>
+          <v-card-text class="text--primary">
+            <h3>Lorem</h3>
+            <p>Lorem ipsum</p>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn>Edit</v-btn>
+            <v-btn
+              class="indigo"
+              dark
+            >Buy</v-btn>
+          </v-card-actions>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
