69c1571d75a0c7f00f4550cc7886f60c3d7f778b
ayurkevich <artur.yurkevich.it@gmail.com>
2020-05-06 22:42:48 -0400

    My ads page

--- ad-board/src/components/Ads/AdList.vue
+++ ad-board/src/components/Ads/AdList.vue
@@ -1,8 +1,37 @@
 <template>
   <v-container>
-    <v-layout>
-      <v-flex>
-        <h1>AdList</h1>
+    <v-layout row>
+      <v-flex xs12 sm6>
+        <h1 class="text--secondary mb-3">My ads</h1>
+        <v-card
+          class="elevation-3 mb-5"
+          v-for="ad in ads"
+          :key="ad.id"
+        >
+          <v-layout>
+            <v-flex xs4>
+              <v-img
+                :src="ad.imageSrc"
+                height="150px"
+              >
+              </v-img>
+            </v-flex>
+            <v-flex xs8>
+              <v-card-text class="text--primary">
+                <h3>{{ad.title}}</h3>
+                <p>{{ad.description}}</p>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  class="indigo"
+                  dark
+                  :to="'/ad/' + ad.id"
+                >Open</v-btn>
+              </v-card-actions>
+            </v-flex>
+          </v-layout>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
@@ -12,7 +41,15 @@
   export default {
     data () {
       return {
-
+        ads: [
+          {
+            id: '1',
+            title: 'Bird',
+            description: 'This is a bird',
+            promo: true,
+            imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+          },
+        ]
       }
     }
   }
