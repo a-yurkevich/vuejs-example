92b4dcd671ddbc1f8dfbae0671b76827a56d107b
ayurkevich <artur.yurkevich.it@gmail.com>
2020-05-08 22:41:00 -0400

    Add new ad (working with db)

--- ad-board/src/components/Ads/NewAd.vue
+++ ad-board/src/components/Ads/NewAd.vue
@@ -51,7 +51,8 @@
           <v-flex xs12>
             <v-spacer></v-spacer>
             <v-btn
-              :disabled="!valid"
+              :loading="loading"
+              :disabled="!valid || loading"
               class="success"
               @click="createAd"
             >Create ad
@@ -73,6 +74,11 @@
         valid: false
       }
     },
+    computed: {
+      loading () {
+        return this.$store.getters.loading
+      }
+    },
     methods: {
       createAd () {
         if (this.$refs.form.validate()) {
@@ -83,6 +89,10 @@
             imageSrc: 'https://www.jomendez.com/wp-content/uploads/2017/03/vuejs.jpg'
           }
           this.$store.dispatch('createAd', ad)
+            .then(() => {
+              this.$router.push('/list')
+            })
+            .catch(() => {})
         }
       }
     }
